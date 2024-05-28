e8c52aa16aba9f6ced2adc9d3b592b8ec8b4e092
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-05-28 07:13:31 -0600

    Servicio creado

---
+++ LanguageLearningAPI/api/services/roleService.py
@@ -0,0 +1,22 @@
+from api.models.role import Role
+from api.serializers.roleSerializer import RoleSerializer
+
+
+class RoleService:
+
+    def list(self):
+
+        try:
+
+            # Obtain all roles
+            roles = Role.objects.all()
+
+            # We serialize the objects
+            serializer = RoleSerializer(roles, many = True)
+
+            return serializer.data
+
+        except ValueError as e:
+
+            # Send the exception
+            raise e
