a3aee7d5ed7bbdd6499564b9514fd9e9920168bb
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-05-28 02:02:24 -0600

    Comentario añadido

--- LanguageLearningAPI/api/models/user.py
+++ LanguageLearningAPI/api/models/user.py
@@ -4,12 +4,14 @@ from api.models.role import Role
 class User(models.Model):
     full_name = models.CharField(max_length = 255)
     username = models.CharField(max_length = 255)
+
+    # It validates that there is a unique email registered
     email = models.EmailField(unique = True)
+    
     password = models.CharField(max_length = 255)
     birth_date = models.DateField()
     country = models.CharField(max_length = 20)
     id_role = models.ForeignKey(Role, on_delete = models.CASCADE, related_name = 'users')
-    #profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
 
     def __str__(self):
         return self.username
