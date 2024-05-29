941deefdf21804534e111f3b59675c8555724b2a
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-05-29 10:29:11 -0600

    Cambio de contraseña por encriptada

--- LanguageLearningAPI/api/migrations/0002_initial_data.py
+++ LanguageLearningAPI/api/migrations/0002_initial_data.py
@@ -6,38 +6,44 @@ def createInitialData(apps, schema_editor):
     User = apps.get_model('api', 'User')
 
     # Nest the database with the roles
-    admin = Role.objects.create(role_name='Administrator')
-    moderator = Role.objects.create(role_name='Moderator')
-    user = Role.objects.create(role_name='User')
+    admin = Role.objects.create(role_name = 'Administrator')
+    moderator = Role.objects.create(role_name = 'Moderator')
+    user = Role.objects.create(role_name = 'User')
 
     # Nest the database with the users
     User.objects.bulk_create([
         User(
-            full_name='Admin User',
-            username='admin',
-            email='admin@email.com',
-            password='12345',
-            birth_date='2024-01-01',  # Use ISO 8601 date format
-            country='United States',
-            id_role=admin
+            full_name = 'Admin User',
+            username = 'admin',
+            email = 'admin@email.com',
+            
+            # Encrypted password: 12345
+            password = '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5',
+            birth_date = '2024-01-01',
+            country = 'United States',
+            id_role = admin
         ),
         User(
-            full_name='Moderator User',
-            username='moderator',
-            email='moderator@email.com',
-            password='12345',
-            birth_date='2024-01-01',
-            country='United States',
-            id_role=moderator
+            full_name = 'Moderator User',
+            username = 'moderator',
+            email = 'moderator@email.com',
+
+            # Encrypted password: 12345
+            password = '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5',
+            birth_date = '2024-01-01',
+            country = 'United States',
+            id_role = moderator
         ),
         User(
-            full_name='Common User',
-            username='user',
-            email='user@email.com',
-            password='12345',
-            birth_date='2024-01-01',
-            country='United States',
-            id_role=user
+            full_name = 'Common User',
+            username = 'user',
+            email = 'user@email.com',
+
+            # Encrypted password: 12345
+            password = '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5',
+            birth_date = '2024-01-01',
+            country = 'United States',
+            id_role = user
         ),
     ])
 
