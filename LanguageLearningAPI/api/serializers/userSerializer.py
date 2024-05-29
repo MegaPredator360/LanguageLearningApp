7c93895bd39763a6034ce324754ad1c40c1eaf67
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-05-29 10:06:36 -0600

    Muestra nombre del role

--- LanguageLearningAPI/api/serializers/userSerializer.py
+++ LanguageLearningAPI/api/serializers/userSerializer.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from api.models.user import User
 
 class UserSerializer(serializers.ModelSerializer):
+
+    # To reflect the name of the role
+    role_name = serializers.CharField(source = 'id_role.role_name', read_only = True)
+
     class Meta:
         model = User
-        fields = ['id', 'full_name', 'username', 'email', 'password', 'birth_date', 'country', 'id_role']
+        fields = ['id', 'full_name', 'username', 'email', 'password', 'birth_date', 'country', 'id_role', 'role_name']
