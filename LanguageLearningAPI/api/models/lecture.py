ca8f3c94078cd24b7d7508c06403b000aa39ae80
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-06-03 07:24:06 -0600

    Tabla relacional añadida

--- LanguageLearningAPI/api/models/lecture.py
+++ LanguageLearningAPI/api/models/lecture.py
@@ -1,4 +1,5 @@
 from django.db import models
+from api.models.topic import Topic
 from api.models.user import User
 from api.models.language import Language
 
@@ -8,6 +9,7 @@ class Lecture(models.Model):
     publish_date = models.DateTimeField(auto_now_add = True)
     user_id = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'users')
     language_id = models.ForeignKey(Language, on_delete = models.CASCADE, related_name = 'languages')
+    topic = models.ManyToManyField(Topic, related_name = 'topics')
 
     def __str__(self):
         return self.title
