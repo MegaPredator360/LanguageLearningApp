e99ddfb946efd34efbcf06427b6f31dc155f179b
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-06-04 12:12:39 -0600

    Cambio de tipo de dato + datos unicos

--- LanguageLearningAPI/api/models/user.py
+++ LanguageLearningAPI/api/models/user.py
@@ -4,15 +4,17 @@ from api.models.role import Role
 class User(models.Model):
     full_name = models.CharField(max_length = 255)
     username = models.CharField(max_length = 255)
-
-    # It validates that there is a unique email registered
-    email = models.EmailField(unique = True)
-    
+    email = models.EmailField()
     password = models.CharField(max_length = 255)
     birth_date = models.DateField()
     country = models.CharField(max_length = 20)
-    active = models.BinaryField()
+    active = models.BooleanField()
     role = models.ForeignKey(Role, on_delete = models.CASCADE, related_name = 'users')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields = ['username', 'email'], name = 'unique_username_email')
+        ]
 
     def __str__(self):
         return self.username
