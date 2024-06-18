51200afcc6fa8966b6d82387fff34c9917624d36
VeronicaCastroMurillo <veronicacastromurillo@gmail.com>
2024-06-17 21:10:40 -0600

    servicio creado

---
+++ LanguageLearningReact/src/Services/userService.tsx
@@ -0,0 +1,21 @@
+import { Login } from "../Interfaces/loginInterface";
+import { ResponseAPI } from "../Interfaces/responseApi";
+import { Environment } from "../environment/environment";
+
+class UserService{
+    urlApi = Environment.endpoint + "user/";
+
+    async Login(req: Login): Promise<ResponseAPI>{
+        const response= await fetch(`${this.urlApi}login`, {
+            method: 'POST',
+            body: JSON.stringify(req),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            }
+            
+        })
+          return response.json()
+    }
+}
+
+export default new UserService
