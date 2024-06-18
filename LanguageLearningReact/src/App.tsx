2b305ee28b6dae90a1df8173ed70892289f26bce
VeronicaCastroMurillo <veronicacastromurillo@gmail.com>
2024-06-17 21:11:00 -0600

    componentes globales creados

--- LanguageLearningReact/src/App.tsx
+++ LanguageLearningReact/src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Home from './Views/home';
+import LoginView from './Views/login';
+import Lecture from './Views/lecture';
 
 function App() {
 
+  const router = createBrowserRouter([
+    {path: "/", element: <Home/>, },
+    {path: "/login", element: <LoginView/>},
+    {path: "/lecture", element: <Lecture/>}
+  ])
+
   return (
     <>
-      <h1>
-        Hola
-      </h1>
+       <div className='background'><RouterProvider router={router}/></div>
+      
     </>
   )
 }
