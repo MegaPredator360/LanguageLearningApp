0d8e280a7be3cd1fe7b861a226c64588da215ace
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-06-20 07:41:51 -0600

    Correccion en el enlace

--- LanguageLearningReact/src/Services/userService.tsx
+++ LanguageLearningReact/src/Services/userService.tsx
@@ -2,19 +2,18 @@ import { Login } from "../Interfaces/loginInterface";
 import { ResponseAPI } from "../Interfaces/responseApi";
 import { Environment } from "../environment/environment";
 
-class UserService{
+class UserService {
     urlApi = Environment.endpoint + "user/";
 
-    async Login(req: Login): Promise<ResponseAPI>{
-        const response= await fetch(`${this.urlApi}login`, {
+    async Login(req: Login): Promise<ResponseAPI> {
+        const response = await fetch(`${this.urlApi}login/`, {
             method: 'POST',
             body: JSON.stringify(req),
             headers: {
                 'Content-type': 'application/json; charset=UTF-8'
             }
-            
         })
-          return response.json()
+        return response.json()
     }
 }
 
