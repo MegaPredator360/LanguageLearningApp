2f440fff496db3a79ea5a6b17259f67bf3ec66c8
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-06-20 09:32:48 -0600

    Validaciones y notificaciones añadidas

--- LanguageLearningReact/src/Views/login.tsx
+++ LanguageLearningReact/src/Views/login.tsx
@@ -1,57 +1,124 @@
 import React from 'react';
-import { Card, Divider, Button, Input, Space, Flex, Checkbox, Form} from 'antd';
-import 'bootstrap/dist/css/bootstrap-grid.css';
-import 'bootstrap/dist/css/bootstrap-utilities.css';
+import { Card, Divider, Button, Input, Space, notification } from 'antd';
+import 'bootstrap/dist/css/bootstrap.css'
 import { UserOutlined } from '@ant-design/icons';
-import type { FormProps } from 'antd';  
 import userService from '../Services/userService';
 import { Login } from '../Interfaces/loginInterface';
 
-
-
 const LoginView: React.FC = () => {
-  const [inputValue, setInputValue] = React.useState("");
+
+  // Formulario
+  const [emailValue, setEmailValue] = React.useState("");
   const [passwordValue, setPasswordValue] = React.useState("");
-  const handleEmailChanges =(e: any)=>{
-    setInputValue(e.target.value);
+
+  // Controlar boardeado de errores de inputs
+  const [emailError, setEmailError] = React.useState(false);
+  const [passwordError, setPasswordError] = React.useState(false);
+
+  // Notificacion
+  const [api, contextHolder] = notification.useNotification();
+
+  // Controlar el cambio de texto
+  const handleEmailChanges = (e: any) => {
+    setEmailValue(e.target.value);
+    setEmailError(false)
   }
 
-  const handlePasswordChanges =(e: any)=>{
+  const handlePasswordChanges = (e: any) => {
     setPasswordValue(e.target.value);
+    setPasswordError(false)
   }
 
-  const loginSubmit = async()=>{
+  const validateForm = () => {
+
+    let emptyInput: Boolean = false
+
+    if (emailValue == '') {
+      setEmailError(true)
+      emptyInput = true
+    }
+
+    if (passwordValue == '') {
+      setPasswordError(true)
+      emptyInput = true
+    }
+
+    return emptyInput
+  }
+
+  // Submit para iniciar sesion
+  const loginSubmit = async () => {
+
+    // Se valida formulario
+    if (validateForm()) {
+
+      // Notificacion
+      api['error']({
+        message: 'Error',
+        description:
+          'There is one or more fields empty.',
+      });
+
+      return
+    }
+
+    // Se crea objeto Login
     const login: Login = {
-      email: inputValue,
+      email: emailValue,
       password: passwordValue
     }
+
+    // Se envia peticion
     await userService.Login(login).then(data => {
-      if(data.status){
+      if (data.status) {
         console.log(data.value);
       }
-      else{
-        console.error(data.msg);
+      else {
+        // Notificacion
+        api['error']({
+          message: 'Error',
+          description: data.msg,
+        });
       }
     });
   }
- 
-  return(    
-  <div className='d-flex justify-content-center align-items-center vh-100'>
-    <Space direction="vertical" size={16}>
-    <Card title={<div style={{textAlign:'center'}}>Login</div>} style={{ width: 300}}>
-     
-    <Input value={inputValue} onChange={handleEmailChanges} placeholder="Email" suffix={<UserOutlined />} />
-    <br />
-    <br />
-    <Input.Password placeholder="Password" value={passwordValue} onChange={handlePasswordChanges} />
-    <br />
-    <br />
-    <Button type="primary" block onClick={loginSubmit}>
-      Login
-    </Button>
-    </Card>
-  </Space>
-  </div>)
+
+  return (
+    <>
+      {contextHolder}
+      <div className='d-flex justify-content-center align-items-center vh-100'>
+        <Space direction="vertical" size={16}>
+          <Card title={<div style={{ textAlign: 'center' }}>Login</div>} style={{ width: 300 }}>
+            <Input
+              value={emailValue}
+              onChange={handleEmailChanges}
+              status={emailError ? 'error' : ''}
+              placeholder="Email"
+              suffix={<UserOutlined />}
+            />
+            <br />
+            <br />
+            <Input.Password
+              placeholder="Password"
+              value={passwordValue}
+              status={passwordError ? 'error' : ''}
+              onChange={handlePasswordChanges}
+            />
+            <br />
+            <br />
+            <Button type="primary" block onClick={loginSubmit}>
+              Login
+            </Button>
+            <Divider />
+            <p className='text-center'>Don't have an account?</p>
+            <Button type="default" block>
+              Register
+            </Button>
+          </Card>
+        </Space>
+      </div>
+    </>
+  )
 };
 
 export default LoginView;
