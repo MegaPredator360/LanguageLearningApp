74a844cbf74258e7c9c4e596c4131dc07d456951
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-07-08 21:53:16 -0600

    Interface de usuario cerada

---
+++ LanguageLearningReact/src/Interfaces/user-interface.tsx
@@ -0,0 +1,12 @@
+export interface User {
+    id: number,
+    full_name: string,
+    username: string, 
+    email: string,
+    password: string, 
+    birth_date: string,
+    country: string,
+    active: boolean,
+    role: number,
+    role_name: string
+}
