47b04cbe7f6aa7ce42420aaadbf96c038d140081
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-07-22 08:44:20 -0600

    Categories created

---
+++ LanguageLearningReact/src/services/category-service.tsx
@@ -0,0 +1,22 @@
+import { Environment } from "../environment/environment";
+import { ResponseAPI } from "../interfaces/response-api-interface";
+
+class CategoryService {
+
+    // Enlace principal
+    urlApi = Environment.endpoint + "category/";
+
+    // Lista de Temas
+    async List(): Promise<ResponseAPI> {
+
+        // Se realiza la solicitud a la API
+        const response = await fetch(`${this.urlApi}list/`, {
+            method: 'GET'
+        })
+
+        // Se retorna la respuesta de la API
+        return response.json()
+    }
+}
+
+export default new CategoryService;
