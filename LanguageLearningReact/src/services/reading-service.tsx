00be59f8ab265748f4505cb34df2cf41fa46b5d3
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-07-25 18:53:27 -0600

    Servicio Creado

---
+++ LanguageLearningReact/src/services/reading-service.tsx
@@ -0,0 +1,34 @@
+import { Environment } from "../environment/environment";
+import { Reading } from "../interfaces/reading-interface";
+import { ResponseAPI } from "../interfaces/response-api-interface";
+
+class ReadingService {
+
+    // Enlace principal
+    urlApi = Environment.endpoint + "reading/";
+
+    // Lista de Lecturas
+    async List(): Promise<ResponseAPI> {
+
+        // Se realiza la solicitud a la API
+        const response = await fetch(`${this.urlApi}list/`, {
+            method: 'GET'
+        })
+
+        // Se retorna la respuesta de la API
+        return response.json()
+    }
+
+    async Create(req: Reading): Promise<ResponseAPI> {
+        const response = await fetch(`${this.urlApi}create/`, {
+            method: 'POST',
+            body: JSON.stringify(req),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            }
+        })
+        return response.json()
+    }
+}
+
+export default new ReadingService;
