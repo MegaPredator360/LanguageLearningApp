9f2fc6586af50b3f60d5b4a011215861bdfa6b88
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-07-25 18:53:12 -0600

    Interfaces Creadas

---
+++ LanguageLearningReact/src/interfaces/reading-interface.tsx
@@ -0,0 +1,19 @@
+import { Tags } from "./tags-interface";
+
+export interface Reading {
+    id: number,
+    title: string,
+    description: string,
+    body: string,
+    publish_date: string,
+    likes: number,
+    dislikes: number,
+    views: number,
+    user: number,
+    user_username: string,
+    language: number,
+    language_name: string,
+    category: number,
+    category_name: string,
+    reading_tags: Tags[]
+}
