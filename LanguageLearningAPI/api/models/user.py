521a1b046bccaac9c390a5126c200a8023d6ace9
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-07-28 16:44:18 -0600

    Se cambio el modelo de usuario por uno base

--- LanguageLearningAPI/api/models/user.py
+++ LanguageLearningAPI/api/models/user.py
@@ -1,15 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 from api.models.role import Role
 
-class User(models.Model):
-    full_name = models.CharField(max_length = 255)
-    username = models.CharField(max_length = 255)
-    email = models.CharField(max_length = 255)
-    password = models.CharField(max_length = 255)
+class User(AbstractUser):
+    full_name = models.CharField(max_length=255)
     birth_date = models.DateField()
-    country = models.CharField(max_length = 20)
-    active = models.BooleanField()
-    role = models.ForeignKey(Role, on_delete = models.CASCADE, related_name = 'user_role')
+    country = models.CharField(max_length=20)
+    active = models.BooleanField(default=True)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, related_name='user_role')
+
+    # Fields to be ignored
+    first_name = None
+    last_name = None
+    is_active = None
+
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.username
