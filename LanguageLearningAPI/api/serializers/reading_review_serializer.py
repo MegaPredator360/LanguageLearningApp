9b254acf61ce75fe5308b4d732b5492564d3b1cf
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-08-13 07:54:22 -0600

    Nombre de Usuario agregado

--- LanguageLearningAPI/api/serializers/reading_review_serializer.py
+++ LanguageLearningAPI/api/serializers/reading_review_serializer.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from api.models.reading_review import ReadingReview
 
 class ReadingReviewSerializer(serializers.ModelSerializer):
+
+    # Other Fields
+    user_username = serializers.CharField(source = 'user.username', read_only = True)
+
     class Meta:
         model = ReadingReview
-        fields = ['id', 'reading', 'user', 'comment', 'publish_date', 'user_rate']
+        fields = ['id', 'reading', 'user', 'user_username', 'comment', 'publish_date', 'user_rate']
