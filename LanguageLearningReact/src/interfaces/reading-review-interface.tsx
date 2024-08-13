64a04f2f8a72e4639998d903191fb02547de5e81
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-08-13 07:55:32 -0600

    Interface de comentario creado

---
+++ LanguageLearningReact/src/interfaces/reading-review-interface.tsx
@@ -0,0 +1,10 @@
+export interface ReadingReview {
+    id: number,
+    reading: number,
+    user: number,
+    user_username: string,
+    comment: string,
+    publish_date: string,
+    user_rate: number,
+    jwt?: string
+}
