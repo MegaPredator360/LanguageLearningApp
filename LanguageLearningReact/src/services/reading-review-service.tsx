14a540c8d038b15bf6b5278e66665d62d02583ba
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-08-13 07:55:48 -0600

    Servicio de Comentario Creado

---
+++ LanguageLearningReact/src/services/reading-review-service.tsx
@@ -0,0 +1,39 @@
+import { Environment } from "../environment/environment";
+import { ReadingReview } from "../interfaces/reading-review-interface";
+import { ResponseAPI } from "../interfaces/response-api-interface";
+
+class ReadingReviewService {
+
+    // Enlace principal
+    urlApi = Environment.endpoint + "reading_review/";
+
+    // Lista de Comentarios
+    async List(): Promise<ResponseAPI> {
+
+        // Se realiza la solicitud a la API
+        const response = await fetch(`${this.urlApi}list/`, {
+            method: 'GET'
+        })
+
+        // Se retorna la respuesta de la API
+        return response.json()
+    }
+
+    // Agregar Comentario
+    async Create(req: ReadingReview): Promise<ResponseAPI> {
+
+        // Se realiza la solicitud a la API
+        const response = await fetch(`${this.urlApi}create/`, {
+            method: 'POST',
+            body: JSON.stringify(req),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            }
+        })
+
+        // Se retorna la respuesta de la API
+        return response.json()
+    }
+}
+
+export default new ReadingReviewService;
