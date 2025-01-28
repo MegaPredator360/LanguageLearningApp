b2c3b6262a8427d2e22705e55d9332f688721323
MegaPredator360 <aaronalfarozamora@gmail.com>
2025-01-28 17:19:23 -0600

    Se añade peticion para actualizar vistas de lecturas

--- LanguageLearningReact/src/services/reading-service.tsx
+++ LanguageLearningReact/src/services/reading-service.tsx
@@ -19,14 +19,28 @@ class ReadingService {
         return response.json()
     }
 
+    // Crear lecturas
     async Create(req: Reading): Promise<ResponseAPI> {
         const response = await fetch(`${this.urlApi}create/`, {
             method: 'POST',
             body: JSON.stringify(req),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8'
-            }
+            credentials: 'include',
+            headers: { 'Content-type': 'application/json; charset=UTF-8' }
         })
+        return response.json()
+    }
+
+    // Actualizar vistas de lectura
+    async UpdateViews(req: Reading): Promise<ResponseAPI> {
+
+        // Se realiza la solicitud a la API
+        const response = await fetch(`${this.urlApi}update_views/`, {
+            method: 'PUT',
+            body: JSON.stringify(req),
+            headers: { 'Content-type': 'application/json; charset=UTF-8' }
+        })
+
+        // Se retorna la respuesta de la API
         return response.json()
     }
 }
