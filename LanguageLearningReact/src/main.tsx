f2d6708aa2c3f34f124d85cb580ff8979a823e2d
MegaPredator360 <aaronalfarozamora@gmail.com>
2025-01-28 17:15:13 -0600

    Scrict Mode comentado

--- LanguageLearningReact/src/main.tsx
+++ LanguageLearningReact/src/main.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+//import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { NotificationProvider } from './components/notification-component.tsx';
 
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <NotificationProvider>
+      <App />
+  </NotificationProvider>,
+)
+
+/*
+// Usa Strict para detectar errores
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <NotificationProvider>
@@ -10,3 +18,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </NotificationProvider>,
   </React.StrictMode>,
 )
+*/
